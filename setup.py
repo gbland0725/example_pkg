86af50a4e788343d8f6e427f4f6d09ad2d9be78e
Garret Bland <gbland@andrew.cmu.edu>
2021-01-26 15:17:35 -0500

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-gbland0725", # Replace with your own username
+    version="0.0.1",
+    author="Garret",
+    author_email="garretbland@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gbland0725/example_pkg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
